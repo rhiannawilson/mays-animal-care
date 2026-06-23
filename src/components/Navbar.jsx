558667328a3681c6ccb887994286fca5012f9ee7
rmay <159880101+rhiannawilson@users.noreply.github.com>
2026-06-23 23:31:05 +1000

    Add hamburger toggle to navbar for mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,28 @@ function Navbar() {
           />
         </a>
 
-        <div className="navbar-nav ms-auto">
-          <a className="nav-link" href="#">Home</a>
-          <a className="nav-link" href="#">Services</a>
-          <a className="nav-link" href="#">About</a>
-          <a className="nav-link" href="#">Contact</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <div className="navbar-nav ms-auto">
+            <a className="nav-link" href="#">Home</a>
+            <a className="nav-link" href="#">Services</a>
+            <a className="nav-link" href="#">About</a>
+            <a className="nav-link" href="#">Contact</a>
+          </div>
         </div>
 
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles/Variables.css'
 import './styles/App.css'
 
