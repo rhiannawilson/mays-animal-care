d7299258647c1e2b94e99c557affe981c0502896
rmay <159880101+rhiannawilson@users.noreply.github.com>
2026-07-04 22:17:28 +1000

    chocco image added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,30 +1,38 @@
 import "../styles/Gallery.css";
 
 import edna from "../assets/images/edna.jpg";
+import chocco from "../assets/images/chocco.jpg";
 
 function Gallery() {
   return (
     <section id="gallery" className="gallery">
       <div className="container">
-
         <h2>Happy Pets</h2>
-
         <p className="gallery-subtitle">
           A few of the wonderful pets I've had the pleasure of caring for.
         </p>
-
+        {/* Gallery - EDNA */}
         <div className="gallery-grid">
-
           <div className="gallery-card">
             <img src={edna} alt="Edna the cat" />
-
             <h3>Edna</h3>
-
             <p>
               Enjoying some sunshine during a home visit.
             </p>
           </div>
 
+
+        {/* Gallery - Chocco */}
+        <div className="gallery-grid">
+          <div className="gallery-card">
+            <img src={chocco} alt="Chocco the Shitzu" />
+            <h3>Chocco</h3>
+            <p>
+              Enjoying my company on the balcony during a boarding stay at my home.
+            </p>
+          </div>
+
+        </div>
         </div>
 
       </div>
